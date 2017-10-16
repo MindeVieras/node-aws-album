97358ecbfcef7dfaeca3af22bf90a9af6ff36909
MindeVieras <wieraz21@gmail.com>
2017-10-16 01:11:35 +0100

    edit album update dates fix

--- models/query.js
+++ models/query.js
@@ -4,14 +4,18 @@ const connection = require('../config/db');
 // Get media by Album ID
 module.exports.getMedia = function(id, limit, cb){
 
-  connection.query(`SELECT * FROM media
-                      WHERE status = 1 AND type_id = ? LIMIT ?`,
+  connection.query(`SELECT
+                      media.*,
+                      media_meta.meta_value AS datetime
+                    FROM media
+                      JOIN media_meta ON media.id = media_meta.media_id AND media_meta.meta_name = 'datetime'
+                    WHERE status = 1 AND type_id = ? LIMIT ?`,
       
     [id,limit], function(err,rows) {
           
       if(err)
         console.log("Error Selecting : %s ",err );
-        //console.log(rows);
+        console.log(rows);
         cb(null, rows);
     }
 
