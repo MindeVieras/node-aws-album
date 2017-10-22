6197a18f5b5003c32e2ab05447fed95024220526
MindeVieras <wieraz21@gmail.com>
2017-10-22 19:44:07 +0100

    transcoder pipeline env var fix

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 
 exports.bucket = process.env.S3_BUCKET || 'images.album.mindelis.com';
 exports.faces_collection = process.env.FACES_COLLECTION || 'album_faces_local';
-exports.transcoder_pipeline = process.env.TRANCODER_PIPELINE || '1508692593579-7zkwqr';
+exports.transcoder_pipeline = process.env.TRANSCODER_PIPELINE || '1508692593579-7zkwqr';
